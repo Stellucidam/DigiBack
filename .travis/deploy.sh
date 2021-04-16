aa1f07d14a679a4b93f344b6c4173b0204325c1e
ClarFleu <clarisse.fleu@gmail.com>
2021-04-16 18:26:35 +0200

    Add all stuff

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd app/build/outputs/apk/productionAPI/debug/
-curl -F document=@app-productionAPI-debug.apk https://api.telegram.org/bot${TL_TOKEN}/sendDocument?chat_id={CHANNEL_ID}
+cd app/build/outputs/apk/debug/
+curl -F document=@*debug.apk https://api.telegram.org/bot${TL_TOKEN}/sendDocument?chat_id=@DigiBackApp
