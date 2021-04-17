5f7c8a951c6968ed78c8d49a4b40ff302591fb5e
ClarFleu <clarisse.fleu@gmail.com>
2021-04-17 11:12:27 +0200

    Change travis stuff

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd app/build/outputs/apk/debug/
-curl -F document=@*debug.apk https://api.telegram.org/bot${TL_TOKEN}/sendDocument?chat_id=@DigiBackApp
+curl -F document=@app-debug.apk https://api.telegram.org/bot{TL_TOKEN}/sendDocument?chat_id=@DigiBackApp
