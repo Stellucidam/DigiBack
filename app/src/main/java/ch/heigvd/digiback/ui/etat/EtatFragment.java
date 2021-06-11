2821fa799f33cf2c897ccd8f5597ada2587c33cd
ClarFleu <clarisse.fleu@gmail.com>
2021-06-11 12:18:22 +0200

    Add paiin level entry

--- app/src/main/java/ch/heigvd/digiback/ui/etat/EtatFragment.java
+++ app/src/main/java/ch/heigvd/digiback/ui/etat/EtatFragment.java
@@ -1,29 +1,23 @@
 package ch.heigvd.digiback.ui.etat;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.PopupMenu;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-import android.view.View.OnTouchListener;
-import android.view.MotionEvent;
 
-import ch.heigvd.digiback.MainActivity;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
-import com.jjoe64.graphview.ValueDependentColor;
-
-import android.graphics.Color;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -36,6 +30,11 @@ import ch.heigvd.digiback.R;
 
 public class EtatFragment extends Fragment {
 
+    private static final String TAG = "EtatFragment";
+    private GraphView graph;
+    private Button changeDate;
+    private Button addPain;
+
     private EtatViewModel etatViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -44,8 +43,67 @@ public class EtatFragment extends Fragment {
                 ViewModelProviders.of(this).get(EtatViewModel.class);
         View root = inflater.inflate(R.layout.fragment_etat, container, false);
 
-        Button changeDate = (Button) root.findViewById(R.id.change_date);
-        Button addPain = (Button) root.findViewById(R.id.add_pain);
+        graph = root.findViewById(R.id.graph);
+        setGraph();
+
+        changeDate = root.findViewById(R.id.change_date);
+        changeDate.setOnClickListener(view -> {
+            // TODO
+        });
+
+        addPain = root.findViewById(R.id.add_pain);
+        addPain.setOnClickListener(view -> {
+            PopupMenu popup = new PopupMenu(getContext(), view);
+            popup.setOnMenuItemClickListener(item -> {
+                Log.d(TAG, "Selected pain : " + item.getTitle());
+                switch (item.getItemId()) {
+                    case R.id.pain_0:
+                        // TODO
+                        return true;
+                    case R.id.pain_1:
+                        // TODO
+                        return true;
+                    case R.id.pain_2:
+                        // TODO
+                        return true;
+                    case R.id.pain_3:
+                        // TODO
+                        return true;
+                    case R.id.pain_4:
+                        // TODO
+                        return true;
+                    case R.id.pain_5:
+                        // TODO
+                        return true;
+                    case R.id.pain_6:
+                        // TODO
+                        return true;
+                    case R.id.pain_7:
+                        // TODO
+                        return true;
+                    case R.id.pain_8:
+                        // TODO
+                        return true;
+                    case R.id.pain_9:
+                        // TODO
+                        return true;
+                    case R.id.pain_10:
+                        // TODO
+                        return true;
+                    default:
+                        return false;
+                }
+            });
+            MenuInflater inflater1 = popup.getMenuInflater();
+            inflater1.inflate(R.menu.add_pain, popup.getMenu());
+            popup.show();
+        });
+
+        return root;
+    }
+
+    private void setGraph() {
+        // TODO make it dynamic
 
         // generate Dates
         final Calendar calendar = Calendar.getInstance();
@@ -61,7 +119,6 @@ public class EtatFragment extends Fragment {
         final Date d0 = calendar.getTime();
 
         // Graph
-        final GraphView graph = (GraphView) root.findViewById(R.id.graph);
         final DataPoint[] points = new DataPoint[] {
                 new DataPoint(d0, 1),
                 new DataPoint(d1, 3),
@@ -73,12 +130,7 @@ public class EtatFragment extends Fragment {
         BarGraphSeries<DataPoint> series = new BarGraphSeries<>(points);
 
         // styling
-        series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
-            @Override
-            public int get(DataPoint data) {
-                return Color.rgb(83, 169, 98);
-            }
-        });
+        series.setValueDependentColor(data -> Color.rgb(83, 169, 98));
         series.setSpacing(10);
 
         graph.addSeries(series);
@@ -100,8 +152,5 @@ public class EtatFragment extends Fragment {
         // as we use dates as labels, the human rounding to nice readable numbers
         // is not necessary
         graph.getGridLabelRenderer().setHumanRounding(false);
-
-        return root;
     }
-
 }
