f9c60b8d26746257a4e626e908975c34777682fa
ClarFleu <clarisse.fleu@gmail.com>
2021-07-15 11:51:21 +0200

    Add pain level to movement

--- app/src/main/java/ch/heigvd/digiback/business/model/movement/Movement.java
+++ app/src/main/java/ch/heigvd/digiback/business/model/movement/Movement.java
@@ -12,19 +12,22 @@ public class Movement {
     private final MovementType type;
     private final Date date;
     private final List<Float> angles;
+    private final int painLevel;
 
     public Movement() {
         type = MovementType.UNKNOWN;
         id = 0;
         date = null;
         angles = new LinkedList<>();
+        painLevel = -1;
     }
 
-    public Movement(int id, MovementType type, Date date, List<Float> angles) {
+    public Movement(int id, MovementType type, Date date, List<Float> angles, int painLevel) {
         this.id = id;
         this.type = type;
         this.date = date;
         this.angles = angles;
+        this.painLevel = painLevel;
     }
 
     public Movement(MovementType type, Date date, List<Float> angles) {
@@ -32,5 +35,14 @@ public class Movement {
         this.type = type;
         this.date = date;
         this.angles = angles;
+        painLevel = -1;
+    }
+
+    public Movement(MovementType type, Date date, List<Float> angles, int painLevel) {
+        this.id = 0;
+        this.type = type;
+        this.date = date;
+        this.angles = angles;
+        this.painLevel = painLevel;
     }
 }
